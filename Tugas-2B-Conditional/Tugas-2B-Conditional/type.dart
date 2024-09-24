da3c250f387f00e9f898a64d52844e5fb141386c
andi <a@students.amikom.ac.id>
2024-09-25 03:17:44 +0700

    Pengumpulan Tugas 2 type dan conditional

---
+++ Tugas-2B-Conditional/Tugas-2B-Conditional/type.dart
@@ -0,0 +1,83 @@
+//Soal nomor 1
+
+// void main() {
+//   var first = 'dart';
+//   var second = 'is';
+//   var third = 'awesome';
+//   var fourth = 'and';
+//   var fifth = 'i';
+//   var sixth = 'love';
+//   var seventh = 'it!';
+
+//   var sentance =
+//       '${first[0].toUpperCase()}${first.substring(1)}$second $third $fourth $fifth $sixth $seventh';
+//   print(sentance);
+// }
+// Pada variabel first, karakter pertama 'd' diubah menjadi huruf kapital menggunakan toUpperCase() dan sisanya diambil dengan substring(1).
+// Variabel-variabel lainnya dihubungkan menggunakan interpolasi string ${}.
+
+//Soal nomor 2
+// void main() {
+//   var sentence = "I am going to be Flutter Developer";
+//   var Words = sentence.split(' ');
+//   var firstWord = Words[0];
+//   var secondWord = Words[1];
+//   var thridWord = Words[2];
+//   var fourthWord = Words[3];
+//   var fifthWord = Words[4];
+//   var sixthWord = Words[5];
+//   var seventhWord = Words[6];
+
+//   print('First Word: ' + firstWord);
+//   print('second Word: ' + secondWord);
+//   print('Thrid Word: ' + thridWord);
+//   print('Fourth Word: ' + fourthWord);
+//   print('Fifth Word: ' + fifthWord);
+//   print('Sixth Word: ' + sixthWord);
+//   print('Seventh Word ' + seventhWord);
+// }
+
+//Soal nomor 3
+// import 'dart:io';
+
+// void main() {
+//   // Meminta input untuk nama depan
+//   stdout.write('Masukkan nama depan: ');
+//   String? firstName = stdin.readLineSync();
+
+//   // Meminta input untuk nama belakang
+//   stdout.write('Masukkan nama belakang: ');
+//   String? lastName = stdin.readLineSync();
+
+//   // Menggabungkan nama depan dan nama belakang
+//   String fullName = '$firstName $lastName';
+
+//   // Output hasil gabungan nama
+//   print('Nama lengkap Anda adalah: $fullName');
+// }
+
+// stdin.readLineSync() digunakan untuk membaca input dari pengguna.
+// stdout.write() digunakan untuk menampilkan pesan tanpa pindah ke baris baru (berbeda dari print() yang langsung pindah ke baris baru).
+// Variabel firstName dan lastName menyimpan input dari pengguna, dan kemudian digabungkan dalam variabel fullName.
+// Hasilnya adalah nama lengkap yang diinput akan ditampilkan di layar.
+
+void main() {
+  int a = 5;
+  int b = 10;
+
+  // Perkalian
+  int hasilPerkalian = a * b;
+  print('$a * $b = $hasilPerkalian');
+
+  // Penjumlahan
+  int hasilPenjumlahan = a + b;
+  print('$a + $b = $hasilPenjumlahan');
+
+  // Pengurangan
+  int hasilPengurangan = a - b;
+  print('$a - $b = $hasilPengurangan');
+
+  // Pembagian
+  double hasilPembagian = a / b;
+  print('$a / $b = $hasilPembagian');
+}
