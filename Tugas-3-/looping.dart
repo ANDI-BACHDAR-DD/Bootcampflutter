7b81798a76589b7fa739510b55070c8f107c47bc
andi <a@students.amikom.ac.id>
2024-09-25 12:59:16 +0700

    Tugas-3 -> Tugas-3A – Looping

---
+++ Tugas-3-/looping.dart
@@ -0,0 +1,59 @@
+//Jawaban nomor 1
+// void main() {
+//   int i = 2;
+
+//   // LOOPING PERTAMA
+//   print('LOOPING PERTAMA');
+//   while (i <= 20) {
+//     print('$i - I love coding');
+//     i += 2;
+//   }
+
+//   // LOOPING KEDUA
+//   i = 20;
+//   print('LOOPING KEDUA');
+//   while (i >= 2) {
+//     print('$i - I will become a mobile developer');
+//     i -= 2;
+//   }
+// }
+
+//Jawaban nomor 2
+// void main() {
+//   for (int i = 1; i <= 20; i++) {
+//     if (i % 2 != 0 && i % 3 == 0) {
+//       print('$i - I Love Coding');
+//     } else if (i % 2 == 0) {
+//       print('$i - Berkualitas');
+//     } else {
+//       print('$i - Santai');
+//     }
+//   }
+// }
+
+//jawaban nomor 3
+// void main() {
+//   int lebar = 8;
+//   int tinggi = 4;
+
+//   for (int i = 0; i < tinggi; i++) {
+//     String baris = '';
+//     for (int j = 0; j < lebar; j++) {
+//       baris += '#';
+//     }
+//     print(baris);
+//   }
+// }
+
+// Jawaban nomor 4
+// void main() {
+//   int tinggi = 7;
+
+//   for (int i = 1; i <= tinggi; i++) {
+//     String baris = '';
+//     for (int j = 1; j <= i; j++) {
+//       baris += '#';
+//     }
+//     print(baris);
+//   }
+// }
