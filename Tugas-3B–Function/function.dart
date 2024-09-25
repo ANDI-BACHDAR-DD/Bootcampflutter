47cd9c0dfa247317cad02754c6b885ece118dff8
andi <a@students.amikom.ac.id>
2024-09-25 13:16:07 +0700

    Tugas-3B–Function

---
+++ Tugas-3B–Function/function.dart
@@ -0,0 +1,57 @@
+//Jawaban Nomor 1
+// String teriak() {
+//   return "Halo Sanbers!";
+// }
+
+// void main() {
+//   print(teriak());
+// }
+
+//Jawaban Nomor 2
+
+// int kalikan(int num1, int num2) {
+//   return num1 * num2;
+// }
+
+// void main() {
+//   var num1 = 12;
+//   var num2 = 4;
+
+//   var hasilKali = kalikan(num1, num2);
+//   print(hasilKali); // 48
+// }
+
+//Jawaban Nomor 3
+// String introduce(String name, int age, String address, String hobby) {
+//   return 'Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!';
+// }
+
+// void main() {
+//   var name = "ANDI BACHDAR DD";
+//   var age = 22;
+//   var address = "jln. MaguwoHarjo Sleman";
+//   var hobby = "Coding in Flutter";
+
+//   var perkenalan = introduce(name, age, address, hobby);
+//   print(perkenalan);
+// }
+
+//Jawaban Nomor 4
+
+// int faktorial(int angka) {
+//   if (angka <= 0) {
+//     return 1;
+//   } else {
+//     int hasil = 1;
+//     for (int i = angka; i > 0; i--) {
+//       hasil *= i;
+//     }
+//     return hasil;
+//   }
+// }
+
+// void main() {
+//   var angka = 6;
+//   var hasilFaktorial = faktorial(angka);
+//   print(hasilFaktorial); // 720
+// }
