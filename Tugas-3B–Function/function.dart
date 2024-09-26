906efeb0272363732e97f5214501f045a1f6fff4
andi <a@students.amikom.ac.id>
2024-09-26 16:48:47 +0700

    Tugas-4–ListMaps

--- Tugas-3B–Function/function.dart
+++ Tugas-3B–Function/function.dart
@@ -55,3 +55,94 @@
 //   var hasilFaktorial = faktorial(angka);
 //   print(hasilFaktorial); // 720
 // }
+// import 'dart:io';
+
+// void main() {
+//   print("Masukkan pssword = ");
+//   String? inputText = stdin.readLineSync()!;
+//   print("Password saya adalah : ${inputText}");
+// }
+import 'dart:io';
+
+// void main() {
+//   print("Masukkan Password = ");
+//   String? inputText = stdin.readLineSync()!;
+//   print("Password saya adalah : ${inputText}");
+// }
+// void main() {
+//   var name = "Sommeng";
+//   var age = 22;
+//   var toDayIsFriday = false;
+
+//   print(name);
+//   print(age);
+//   print(toDayIsFriday);
+// }
+//
+
+// void main() {
+//   var sentence = 'I am going to be Flutter Developer';
+
+//   var Word = sentence.split(' ');
+
+//   var firstWord = Word[0];
+//   var secondword = Word[1];
+//   var thridword = Word[2];
+//   var forthword = Word[3];
+//   var fifthword = Word[4];
+//   var sixthword = Word[5];
+//   var seventhword = Word[6];
+
+//   print('FirstWord:' + firstWord);
+//   print('SecondWord:' + secondword);
+//   print('ThridWord:' + thridword);
+//   print('Forthword:' + forthword);
+//   print('fifthWord:' + fifthword);
+//   print('SixthWord:' + sixthword);
+//   print('SeventhWord:' + seventhword);
+// }
+
+// void main() {
+//   stdout.write('Masukkan nama depan : ');
+//   String? namaDepan = stdin.readLineSync();
+
+//   stdout.write('Masukkan nama tengah : ');
+//   String? namaBelakang = stdin.readLineSync();
+
+//   String Fullname = '$namaDepan $namaBelakang';
+
+//   print('Nama lengkap anda adalah : $Fullname ');
+// }
+
+// void main() {
+//   int a = 10;
+//   int b = 15;
+
+//   //perkalian
+//   int hasilperkalian = a * b;
+//   print('$a * $b = $hasilperkalian ');
+
+//   //prnjumlahan
+//   int hasilPenjumlahan = a + b;
+//   print('$a + $b = $hasilPenjumlahan');
+
+//   //pengurangan
+//   int hasilPengurangan = a - b;
+//   print('$a - $b = $hasilPengurangan');
+
+//   //pembagian
+//   double hasilPembagian = a / b;
+//   print('$a / $b = $hasilPembagian');
+// }
+
+void main() {
+  stdout.write('Masukkan nama depan anda : ');
+  String? namaDepan = stdin.readLineSync();
+
+  stdout.write('Masukkan nama belakang anda : ');
+  String? namaBelakang = stdin.readLineSync();
+
+  String fullname = '$namaDepan $namaBelakang';
+
+  print('nama lengkap anda adalah = $fullname ');
+}
